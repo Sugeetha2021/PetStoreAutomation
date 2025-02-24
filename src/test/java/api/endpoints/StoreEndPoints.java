2cc2bff8c966290f60813277d70c46df2f9de076
sugeetha <sugeetha.t2021@gmail.com>
2025-02-24 16:29:22 +0530

    store module code commit

---
+++ src/test/java/api/endpoints/StoreEndPoints.java
@@ -0,0 +1,72 @@
+package api.endpoints;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.ResourceBundle;
+
+import api.payload.Store;
+import api.payload.User;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class StoreEndPoints {
+
+	// method created for getting URL's from properties file
+	static ResourceBundle getURL()
+	{
+		ResourceBundle routes= ResourceBundle.getBundle("routes"); // Load properties file  // name of the properties file
+		return routes;
+	}
+
+	public static Response RetunPetInventory()
+	{
+		String getstore_url=getURL().getString("getstore_url");
+		
+		
+		Response response=given()
+						
+		.when()
+			.get(getstore_url);
+			
+		return response;
+	}
+	public static Response PlaceOrderForPet(Store payload)
+	{
+		String poststore_url=getURL().getString("poststore_url");
+		
+		
+		Response response=given()
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+			.body(payload)
+		.when()
+			.post(poststore_url);
+			
+		return response;
+	}
+	public static Response readOrderByorderID(int orderId)
+	{
+		String getstoreOrder_url=getURL().getString("getstoreorder_url");
+		
+		
+		Response response=given()
+				.pathParam("orderId",2)
+						
+		.when()
+			.get(getstoreOrder_url);
+			
+		return response;
+	}
+	public static Response deleteOrderById(int orderId)
+	{
+		String deletestoreOrder_url=getURL().getString("deletestore_url");
+		
+		Response response=given()
+				.pathParam("orderId",10)
+		.when()
+			.delete(deletestoreOrder_url);
+			
+		return response;
+	}
+	
+}
--- src/test/java/api/test/StoreTest.java
+++ src/test/java/api/test/StoreTest.java
@@ -1,7 +1,95 @@
 package api.test;
 
-public class StoreTest {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-	//yet to implement
+import com.github.javafaker.Faker;
+
+import api.endpoints.StoreEndPoints;
+import api.endpoints.UserEndPoints2;
+import api.payload.Store;
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class StoreTest {
 	
+	Faker faker;
+	Store storePayload;
+	
+	public Logger logger; // for logs
+	
+	@BeforeClass
+	public void setup()
+	{
+		faker=new Faker();
+		storePayload=new Store();
+		
+		
+		storePayload.setId(faker.idNumber().hashCode());
+		storePayload.setPetId(faker.number().hashCode());
+		storePayload.setQuantity(faker.number().hashCode());
+		storePayload.setShipDate(faker.date().hashCode());
+		storePayload.setStatus("placed");
+		storePayload.setComplete("true");
+				
+		//logs
+		logger= LogManager.getLogger(this.getClass());
+		
+		logger.debug("debugging.....");
+		
+	}
+	
+
+
+	@Test(priority=1)
+	public void testGetUserByName()
+	{
+		
+		Response response= StoreEndPoints.RetunPetInventory();
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(),200);
+			
+	}
+	
+	@Test(priority=2)
+	public void testPostUser()
+	{
+		logger.info("********** Creating user  ***************");
+		Response response=StoreEndPoints.PlaceOrderForPet(storePayload);
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(),200);
+		
+		logger.info("**********User is creatged  ***************");
+			
+	}
+
+	@Test(priority=3)
+	public void testGetUserByOrderID()
+	{
+        int orderId = this.storePayload.getId();  // Ensure this retrieves a valid order ID
+
+        Response response = StoreEndPoints.readOrderByorderID(orderId);
+        response.then().log().all();
+        
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+	@Test(priority=4)
+	public void deleteOrderById()
+	{
+		logger.info("**********   Deleting User  ***************");
+		
+		int orderId = this.storePayload.getId();  // Ensure this retrieves a valid order ID
+
+        Response response = StoreEndPoints.deleteOrderById(orderId);
+        response.then().log().all();
+        
+        Assert.assertEquals(response.getStatusCode(), 404);
+		
+		logger.info("********** User deleted ***************");
+	}
 }
